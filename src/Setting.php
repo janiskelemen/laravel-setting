6b521170c75c700121a546f36fd30e88acf46c52
Janis Kelemen <jak@spacemates.io>
2020-01-22 12:18:52 +0100

    Return key as fallback for default if no default_value is present

--- src/Setting.php
+++ src/Setting.php
@@ -39,9 +39,10 @@ class Setting
      *
      * @return void
      */
-    public function default($key)
+    public function
+    default($key)
     {
-        return is_array(config('setting.'.$key)) ? config('setting.'.$key.'.default_value') : config('setting.'.$key);
+        return is_array(config('setting.' . $key)) ? config('setting.' . $key . '.default_value', 'setting.' . $key) : config('setting.' . $key);
     }
 
     /**
@@ -176,18 +177,18 @@ class Setting
         } else {
             $main_key = $key;
         }
-        if ($this->cache->has($main_key.'@'.$this->lang)) {
-            $setting = $this->cache->get($main_key.'@'.$this->lang);
+        if ($this->cache->has($main_key . '@' . $this->lang)) {
+            $setting = $this->cache->get($main_key . '@' . $this->lang);
         } else {
             $setting = $this->storage->retrieve($main_key, $this->lang);
-            if (! is_null($setting)) {
+            if (!is_null($setting)) {
                 $setting = $setting->value;
             }
             $setting_array = json_decode($setting, true);
             if (is_array($setting_array)) {
                 $setting = $setting_array;
             }
-            $this->cache->add($main_key.'@'.$this->lang, $setting, 60);
+            $this->cache->add($main_key . '@' . $this->lang, $setting, 60);
         }
 
         return $setting;
@@ -204,8 +205,8 @@ class Setting
         } else {
             $this->storage->store($main_key, $value, $this->lang);
         }
-        if ($this->cache->has($main_key.'@'.$this->lang)) {
-            $this->cache->forget($main_key.'@'.$this->lang);
+        if ($this->cache->has($main_key . '@' . $this->lang)) {
+            $this->cache->forget($main_key . '@' . $this->lang);
         }
     }
 
@@ -220,8 +221,8 @@ class Setting
         if (strpos($key, '.') !== false) {
             $setting = $this->getSubValue($key);
         } else {
-            if ($this->cache->has($key.'@'.$this->lang)) {
-                $setting = $this->cache->get($key.'@'.$this->lang);
+            if ($this->cache->has($key . '@' . $this->lang)) {
+                $setting = $this->cache->get($key . '@' . $this->lang);
             } else {
                 $setting = $this->storage->retrieve($key, $this->lang);
             }
@@ -239,7 +240,7 @@ class Setting
     protected function forgetByKey($key)
     {
         $this->storage->forget($key, $this->lang);
-        $this->cache->forget($key.'@'.$this->lang);
+        $this->cache->forget($key . '@' . $this->lang);
     }
 
     /**
